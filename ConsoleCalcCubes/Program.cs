7bf1dbf6963ad8b809b2c191cd9e48d64c56a9c8
Peter <>
2023-08-06 09:14:01 +0200

    start: cubes-console-app

---
+++ ConsoleCalcCubes/Program.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+
+namespace ConsoleCalcCubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello,  Initial Commit!");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
